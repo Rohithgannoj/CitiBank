5dfc76cd38e5974d1c396a582f689db59e3278ab
TL <TL@gmail.com>
2022-05-02 17:48:20 +0530

    Dev Added main method in Test.java

--- DemoGit1/src/com/nt/test/Test.java
+++ DemoGit1/src/com/nt/test/Test.java
@@ -1,5 +1,12 @@
 package com.nt.test;
 
+import com.nt.beans.mainApp;
+
 public class Test {
+	public static void main(String[] args) {
+		mainApp m=new mainApp();
+		
+		System.out.println(m.hashCode());
+	}
 
 }
